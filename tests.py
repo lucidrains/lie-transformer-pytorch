4f5c8c95820893f33d690ac3d7a84edc8b54f9c1
Phil Wang <lucidrains@gmail.com>
2021-02-02 16:59:51 -0800

    add one test

--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
+import torch
+from lie_transformer_pytorch import LieTransformer
 
-def test_equivariance():
-    assert True
+def test_transformer():
+    model = LieTransformer(
+        dim = 512,
+        depth = 1,
+        loc_attn = True
+    )
+
+    feats = torch.randn(1, 64, 512)
+    coors = torch.randn(1, 64, 3)
+    mask = torch.ones(1, 64).bool()
+
+    out = model(feats, coors, mask = mask)
+    assert out.shape == (1, 256, 512), 'transformer runs'
